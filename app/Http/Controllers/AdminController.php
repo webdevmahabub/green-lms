1c7cb725cf368af48cb614ef83154305dde7a5b0
webdevmahabub <mahbubbd926@gmail.com>
2023-11-15 13:17:32 +0600

    Admin Profile & Image Update Part 4

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -29,4 +29,27 @@ class AdminController extends Controller
         $profileData = User::find($id);
         return view('admin.admin_profile_view',compact('profileData'));
     }// End Method
+
+    public function AdminProfileStore(Request $request){
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+           $file = $request->file('photo');
+           $filename = date('YmdHi').$file->getClientOriginalName();
+           $file->move(public_path('upload/admin_images'),$filename);
+           $data['photo'] = $filename; 
+        }
+
+        $data->save();
+
+        return redirect()->back();
+
+    }// End Method
 }
