416c6746a53bf892e39f9c578e58ceb692b694dc
webdevmahabub <mahbubbd926@gmail.com>
2023-11-15 13:21:36 +0600

    Adding Toster In For View Message

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -25,7 +25,10 @@
 	<link rel="stylesheet" href="{{ asset('backend/assets/css/dark-theme.css') }}"/>
 	<link rel="stylesheet" href="{{ asset('backend/assets/css/semi-dark.css') }}"/>
 	<link rel="stylesheet" href="{{ asset('backend/assets/css/header-colors.css') }}"/>
-	<title>Rocker - Bootstrap 5 Admin Dashboard Template</title>
+
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+	
+	<title>Admin Dashboard</title>
 </head>
 
 <body>
@@ -170,6 +173,28 @@
 	<script>
 		new PerfectScrollbar(".app-container")
 	</script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+ @if(Session::has('message'))
+ var type = "{{ Session::get('alert-type','info') }}"
+ switch(type){
+    case 'info':
+    toastr.info(" {{ Session::get('message') }} ");
+    break;
+    case 'success':
+    toastr.success(" {{ Session::get('message') }} ");
+    break;
+    case 'warning':
+    toastr.warning(" {{ Session::get('message') }} ");
+    break;
+    case 'error':
+    toastr.error(" {{ Session::get('message') }} ");
+    break; 
+ }
+ @endif 
+</script>
 </body>
 
 </html>
