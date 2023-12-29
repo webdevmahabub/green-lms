4c3c1e3e29eacad836e473c239b7ed61d7e9b74e
webdevmahabub <mahbubbd926@gmail.com>
2023-12-30 05:36:17 +0600

    student  subject  api

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Sclass;
+use App\Models\Subject;
 use Carbon\Carbon;
 use Illuminate\Support\Carbon as SupportCarbon;
 
@@ -58,12 +59,81 @@ class ApiController extends Controller
         return response('Class Update Successfully');
     } // End Method
 
-    public function Delete($id){
+    public function Delete($id)
+    {
 
         Sclass::findOrfail($id)->delete();
 
         return response('Student Delete Successfully');
+    } // End Method
 
-  } // End Method
+
+
+    public function SubjectStore(Request $request)
+    {
+        $validationData = $request->validate([
+            'class_id' => 'required',
+            'subject_name' => 'required|unique:subjects|min:2|max:20',
+            'subject_code' => 'required'
+        ]);
+
+        Subject::insert([
+            'class_id' => $request->class_id,
+            'subject_name' => $request->subject_name,
+            'subject_code' => $request->subject_code,
+            'created_at' => Carbon::now(),
+        ]);
+
+        return response('Student subject insert successfully');
+    } // End Method
+
+
+    public function IndexSubject()
+    {
+
+        $subject = Subject::latest()->get();
+
+        return response()->json($subject);
+    } // End Method
+
+
+
+    public function subjectEdit($id)
+    {
+
+        $edit =   Subject::findOrFail($id);
+
+        return  response()->json($edit);
+    }
+    // End method
+
+    public function subjectUpdate(Request $request, $id)
+    {
+
+
+        $validationData = $request->validate([
+
+            'class_id' => 'required',
+            'subject_name' => 'required|unique:subjects',
+            'subject_code' => 'required',
+        ]);
+        Subject::findOrFail($id)->Update([
+            'class_id' => $request->class_id,
+            'subject_name' => $request->subject_name,
+            'subject_code' => $request->subject_code,
+        ]);
+        return response('subject update successfully');
+    } // End 
+
+
+    public function subjectDelete($id)
+    {
+        Subject::findOrFail($id)->delete();
+
+        return response('subject delete successfully');
+    } // End Method
+
+
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,31 @@ use App\Http\Controllers\Api\ApiController;
 
 
 // student class route api
-Route::get('/class',[ApiController::class, 'index']);
-Route::post('/class/store',[ApiController::class, 'Store']);
-Route::get('/class/edit/{id}',[ApiController::class, 'Edit']);
-Route::post('/class/update/{id}',[ApiController::class, 'Update']);
-Route::get('/class/delete/{id}',[ApiController::class, 'Delete']);
+Route::get('/class', [ApiController::class, 'index']);
+Route::post('/class/store', [ApiController::class, 'Store']);
+Route::get('/class/edit/{id}', [ApiController::class, 'Edit']);
+Route::post('/class/update/{id}', [ApiController::class, 'Update']);
+Route::get('/class/delete/{id}', [ApiController::class, 'Delete']);
+
+
+
+
+// student  subject route api
+Route::get('/subject', [ApiController::class, 'IndexSubject']);
+Route::post('/subject/store', [ApiController::class, 'SubjectStore']);
+Route::get('/subject/edit/{id}', [ApiController::class, 'subjectEdit']);
+Route::post('/subject/update/{id}', [ApiController::class, 'subjectUpdate']);
+Route::get('/subject/delete/{id}', [ApiController::class, 'subjectDelete']);
+
+
+
+
+
+
+
+
+
+
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
